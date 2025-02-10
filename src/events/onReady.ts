fbad19d0662f05e25f7ec5e8f50616144a217d7f
0xRickJames <0xrickjames@gmail.com>
2025-02-10 12:37:46 -0500

    register commands on start

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -1,21 +1,28 @@
 import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
+import { Routes } from "discord-api-types/v10"; // Updated to v10
 import { Client } from "discord.js";
 import { CommandList } from "../commands/_CommandList";
 
 export const onReady = async (BOT: Client) => {
-  const rest = new REST({ version: "9" }).setToken(
+  console.log(`Logged in as ${BOT.user?.tag}`);
+
+  const rest = new REST({ version: "10" }).setToken(
     process.env.BOT_TOKEN as string
   );
 
-  const commandData = CommandList.map((command) => command.data.toJSON());
+  try {
+    console.log("Registering slash commands...");
 
-  await rest.put(
-    Routes.applicationCommands(
-      BOT.user!.id
-    ),
-    { body: commandData }
-  );
+    const commandData = CommandList.map((command) => command.data.toJSON());
+
+    await rest.put(Routes.applicationCommands(BOT.user!.id), {
+      body: commandData,
+    });
+
+    console.log("Successfully registered all commands.");
+  } catch (error) {
+    console.error("Error registering commands:", error);
+  }
 
   console.log("RPG Bot Online!");
 };
